b03b3839355f5d09f514dd4318f4dd5bee5255ad
benjamin amakye <bamakye20@gmail.com>
2022-02-06 21:46:04 -0500

    another one

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -17,13 +17,22 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: "'product_tag'",
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tags_on_product'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: 'product_tag',
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'products_with_tag'
 });
+
 
 module.exports = {
   Product,
