58201b86c5d157551cc6e178f0c37c6937d1f4e5
benjamin amakye <bamakye20@gmail.com>
2022-02-06 21:10:33 -0500

    sixth commit

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,12 +7,32 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [{model: Category}, {model: Tag}],
+  })
+  .then(data => {
+    res.status(200).json(data)
+  })
+  .catch(err => {
+    console.log(err);
+    res.send('Request failed.')
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id, {
+    include: [{model: Category}, {model: Tag}],
+  })
+  .then(data => {
+    res.status(200).json(data)
+  })
+  .catch(err => {
+    console.log(err);
+    res.send('Request failed.')
+  })
 });
 
 // create new product
@@ -91,6 +111,20 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: { id: req.params.id }
+    })
+
+    .then(prodData => {
+      if (!prodData) {
+        res.status(404).json({ message: 'There is no other product associated with this id!' });
+        return;
+      }
+      res.json(prodData);
+    })
+    .catch (err => {
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
